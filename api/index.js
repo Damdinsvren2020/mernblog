771d5879738a83fa88b6fa6c33f2d9ddb13436ad
damdinsvren <damdinsvren9548@gmail.com>
2024-01-31 11:54:24 +0900

    bvrtgeh heseg hiilee

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -11,7 +13,10 @@ mongoose
     console.log();
   });
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
