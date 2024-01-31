98a057cc93d902cb05f837e7b29947d82f0112b6
damdinsvren <damdinsvren9548@gmail.com>
2024-01-31 10:43:26 +0900

    node js mongo db tei holbogdloo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongo db connection");
+  })
+  .catch((err) => {
+    console.log();
+  });
 const app = express();
 
 app.listen(3000, () => {
