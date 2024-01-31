c1526890da2e4e7d2dd70849443d61c7734cb306
damdinsvren <damdinsvren9548@gmail.com>
2024-01-31 10:34:37 +0900

    header bolon node js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import SingUp from "./pages/SignUp";
+import Header from "./Components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
