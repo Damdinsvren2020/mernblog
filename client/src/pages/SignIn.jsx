1ff03a32b982747f8e59e21eda6649362f045019
damdinsvren <damdinsvren9548@gmail.com>
2024-02-01 21:13:24 +0900

    redux nemlee

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,104 +1,107 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 export default function Signin() {
-  const [formdata, setFormData] = useState({});
-  const [errorMessages, setErrorMessages] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({ ...formdata, [e.target.id]: e.target.value.trim() });
-    console.log(formdata);
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formdata.email || !formdata.password) {
-      return setErrorMessages("Талбаруудыг бөглөнө үү !!!");
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure("Талбаруудыг бөглөнө үү"));
     }
     try {
-      setLoading(true);
-      setErrorMessages(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formdata),
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessages(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessages(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
     <div className="min-h-screen mt-20">
-      <div
-        className="gap-4 flex p-3 max-w-3xl mx-auto flex-col md:flex-row 
-      md:items-center"
-      >
-        <div className="">
-          <Link to={"/"} className="font-bold dark:text-white text-4xl">
-            <span
-              className="px-2 py-1 bg-gradient-to-r from-indigo-500
-        via-purple-500 to-pink-500 rounded-lg text-white
-        "
-            >
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Test's
             </span>
             Blog
           </Link>
-          <p className="text-sm mt-5">This is demo project</p>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign in with your email and password
+            or with Google.
+          </p>
         </div>
+        {/* right */}
+
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <div className="">
+            <div>
               <Label value="Your email" />
               <TextInput
-                type={"text"}
+                type="email"
                 placeholder="name@company.com"
                 id="email"
                 onChange={handleChange}
               />
             </div>
-            <div className="">
+            <div>
               <Label value="Your password" />
               <TextInput
-                type={"password"}
-                placeholder="*********"
+                type="password"
+                placeholder="**********"
                 id="password"
                 onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone={"purpleToPink"}
+              gradientDuoTone="purpleToPink"
               type="submit"
               disabled={loading}
             >
               {loading ? (
                 <>
-                  <Spinner size={"sm"} />
-                  <span className="pl-3">Loading ...</span>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
                 </>
               ) : (
-                "Sign in"
+                "Sign In"
               )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
-            <span>Dont have an account ?</span>
-            <Link to={"/sign-up"} className="text-blue-500">
-              Sign up
+            <span>Dont Have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
             </Link>
           </div>
-          {errorMessages && (
-            <Alert className="mt-5" color={"failure"}>
-              {errorMessages}
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
             </Alert>
           )}
         </div>
